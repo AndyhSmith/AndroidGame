1bbfe14a1d762510bc27c3c969403c4cbe8b857d
AndyhSmith <andy.h.smith@gmail.com>
2022-03-07 17:44:31 -0700

    setting up fps

---
+++ app/src/main/java/com/example/androidgame/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.androidgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set window to fullscreen.
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        // Set content view to game
+        setContentView(new Game(this));
+
+    }
+}
